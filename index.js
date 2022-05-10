3b807ebbd8568b39449a800dacd1a00c6448a3d6
brett hockridge <bretthockridge98@gmail.com>
2022-05-09 21:35:40 -0700

    started manager questions on index.js

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const generate = require("./src/generateHTML");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+const employees = [];
+
+// Manager Questions
+function managerQuestions() {
+    return (
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the team manager's name?",
+                validate: (name) => {
+                    if (name) {
+                        return true;
+                    } else {
+                        console.log("Manager's name is required.");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+            }
+        ])
+    )
+}
