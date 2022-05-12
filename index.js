aafbcbef64ce8ca5aa674c1b5a0cfc54f9d87635
brett hockridge <bretthockridge98@gmail.com>
2022-05-11 20:07:49 -0700

    finished index.js file

--- index.js
+++ index.js
@@ -107,8 +107,101 @@ function engineerQuestions() {
                 },
             },
             {
-                
-            }
+              type: "input",
+              name: "email",
+              message: "What is the engineer's email address?",
+              validate: (email) => {
+                  valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                  if (valid) {
+                      return true;
+                  } else {
+                      console.log("Enter a valid email address.");
+                      return false;
+                  }
+              },  
+            },
+            {
+              type: "input",
+              name: "github",
+              message: "What is the engineer's github username?",
+              validate: (githubInput) => {
+                  if (githubInput) {
+                      return true;
+                  } else {
+                      console.log("Include engineer's github information to proceed.");
+                      return false;
+                  }
+              },  
+            },
         ])
-    )
+    );
+}
+
+function internQuestions() {
+    return (
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the intern's name?",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Include intern's name to procees");
+                        return false;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the Intern's employee ID?",
+                validate: (idInput) => {
+                    if (idInput) {
+                        return false;
+                    } else {
+                        console.log("Enter intern's employee ID.");
+                        return true;
+                    }
+                },
+
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email address?",
+                validate: (email) => {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                    if (valid) {
+                        return true;
+                    } else {
+                        console.log("Enter a valid email address.");
+                        return false;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is the name of the intern's school?",
+                validate: (schoolInput) => {
+                    if (schoolInput) {
+                        return true;
+                    } else {
+                        console.log("Include intern's school.");
+                        return false;
+                    }
+                },
+            },
+        ])
+    );
+}
+
+function buildTeam() {
+    fs.writeFileSync(
+        path.join(path.resolve(_dirname, 'dist'), "index.html"),
+        generate(employees),
+        "utf-8"
+    );
 }
